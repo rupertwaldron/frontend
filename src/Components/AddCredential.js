89e0bd4cb00f05d11139599bc0f29a2ed9ad2353
rupertwaldron <rupert.waldron@yahoo.co.uk>
2020-07-18 09:29:45 +0100

    Updated to add fullpath for pcf

--- src/Components/AddCredential.js
+++ src/Components/AddCredential.js
@@ -28,7 +28,7 @@ export default function AddCredential() {
     const [message, setMessage] = React.useState("Nothing saved in the session");
 
     async function sampleFunc(toInput) {
-        const response = await fetch("/credentials", {
+        const response = await fetch("https://springcert-brave-manatee-ij.cfapps.io/credentials", {
             method: "POST", // *GET, POST, PUT, DELETE, etc.
             mode: "cors", // no-cors, *cors, same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
