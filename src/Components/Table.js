3b84740abaaaf310d76d921745eb97336193562b
rupertwaldron <rupert.waldron@yahoo.co.uk>
2020-10-03 17:03:11 +0100

    Update for new service

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -25,7 +25,7 @@ export default function SimpleTable() {
     const [isLoading, setIsLoading] = React.useState(true);
 
     async function sampleFunc() {
-        let response = await fetch("https://springcert-brave-manatee-ij.cfapps.io/credentials", {
+        let response = await fetch("https://credential-manager-responsive-tiger-ja.cfapps.io/credentials", {
             method: "GET", // *GET, POST, PUT, DELETE, etc.
             mode: "cors", // no-cors, *cors, same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
--- src/Components/UpdateCredential.js
+++ src/Components/UpdateCredential.js
@@ -30,7 +30,7 @@ export default function UpdateCredential(props) {
     const [message, setMessage] = React.useState("Nothing saved in the session");
 
     async function sampleFunc(toInput) {
-        const response = await fetch(`https://springcert-brave-manatee-ij.cfapps.io/credentials/` + uuid, {
+        const response = await fetch(`https://credential-manager-responsive-tiger-ja.cfapps.io/credentials/` + uuid, {
             method: "PUT", // *GET, POST, PUT, DELETE, etc.
             mode: "cors", // no-cors, *cors, same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -49,7 +49,7 @@ export default function UpdateCredential(props) {
     }
 
     async function handleDelete() {
-        const response = await fetch(`https://springcert-brave-manatee-ij.cfapps.io/credentials/` + uuid, {
+        const response = await fetch(`https://credential-manager-responsive-tiger-ja.cfapps.io/credentials/` + uuid, {
             method: "DELETE", // *GET, POST, PUT, DELETE, etc.
             mode: "cors", // no-cors, *cors, same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
